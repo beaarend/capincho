4170686244e1d2c434f3ee2ea703165bce550ccc
Andersonsr <andersonsr2014@outlook.com>
2024-11-03 10:42:20 -0300

    slurm install dependencies

--- text_embeddings.sh
+++ text_embeddings.sh
@@ -20,9 +20,24 @@ OUTPUT=$3
 module load python/3.8.0
 module load cuda/11.6
 
-# Install dependencies from requirements.txt
+# Install dependencies one by one to check specific err logs
+pip3 install git+https://github.com/openai/CLIP.git
 pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install huggingface-hub==0.25.1
+pip3 install matplotlib==3.7.5
+pip3 install numpy==1.24.3
+pip3 install open_clip_torch==2.29.0
+pip3 install pandas==2.0.3
+pip3 install peft==0.13.0
+pip3 install pillow==10.4.0
+pip3 install pycocoevalcap==1.2
+pip3 install pycocotools==2.0.7
+pip3 install tokenizers==0.20.0
+pip3 install torch==2.4.1
+pip3 install torchvision==0.19.1
+pip3 install tqdm==4.66.5
+pip3 install transformers==4.45.1
+pip3 install openpyxl
 
 # Run the Python script
 python textLoader.py --path $PATH --model $MODEL --output $OUTPUT
