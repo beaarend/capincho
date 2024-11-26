3a64168f687552b735b0ac10dd556bcabbedaa63
Andersonsr <andersonsr2014@outlook.com>
2024-11-26 20:12:48 -0300

    mapper fix

--- mapping.py
+++ mapping.py
@@ -2,9 +2,8 @@ import torch.nn as nn
 
 
 class Mapper(nn.Module):
-    def __init__(self, input_size, token_length, output_n, device):
+    def __init__(self, input_size, token_length, output_n):
         super(Mapper, self).__init__()
-        self.device = device
         self.model = nn.Sequential(
             nn.Linear(input_size, (token_length * output_n) // 2),
             nn.LeakyReLU(),
@@ -12,12 +11,6 @@ class Mapper(nn.Module):
             nn.LeakyReLU())
 
     def forward(self, x):
-        device = x.get_device()
-        x = self.model(x.to(self.device()))
-        if device >= 0:
-            x = x.to(f'cuda:{device}')
-        else:
-            x = x.to(f'cpu')
-        return x
+        return self.model(x)
 
 
