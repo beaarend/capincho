a66bb9585ba5306ad97537e41c9eb8ec770bf7a5
beaarend <beaarend@gmail.com>
2025-04-10 09:56:32 -0300

    feat: add lora layers to mapper

--- mapping.py
+++ mapping.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+from lora_layers import LoRALinear
 
 class Mapper(nn.Module):
     def __init__(self, input_size, token_length, output_n):
@@ -14,4 +14,15 @@ class Mapper(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+class MapperLoRA(nn.Module):
+    def __init__(self, input_size, token_length, output_n, r=16, alpha=32):
+        super(MapperLoRA, self).__init__()
+        self._tied_weights_keys = None
+        self.model = nn.Sequential(
+            LoRALinear(input_size, (token_length * output_n) // 2, r=r, alpha=alpha),
+            nn.LeakyReLU(),
+            LoRALinear((token_length * output_n) // 2, (token_length * output_n), r=r, alpha=alpha),
+            nn.LeakyReLU())
 
+    def forward(self, x):
+        return self.model(x)
