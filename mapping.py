a5689d79989f3b4c9d88e4c7e319ccde13d1a17a
beaarend <beaarend@gmail.com>
2025-04-14 15:42:34 -0300

    fix: fix linearlora layer

--- mapping.py
+++ mapping.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from lora_layers import LoRALinear
+from lora_layers import LinearLoRA
 
 class Mapper(nn.Module):
     def __init__(self, input_size, token_length, output_n):
@@ -15,13 +15,17 @@ class Mapper(nn.Module):
         return self.model(x)
 
 class MapperLoRA(nn.Module):
-    def __init__(self, input_size, token_length, output_n, r=16, alpha=32):
+    def __init__(self, input_size, token_length, output_n, r=2, alpha=32):
         super(MapperLoRA, self).__init__()
         self._tied_weights_keys = None
+
+        existing_linear_layer_1 = nn.Linear(input_size, (token_length * output_n) // 2)
+        existing_linear_layer_2 = nn.Linear((token_length * output_n) // 2, (token_length * output_n))
+
         self.model = nn.Sequential(
-            LoRALinear(input_size, (token_length * output_n) // 2, r=r, alpha=alpha),
+            LinearLoRA(existing_linear_layer_1, r=r, alpha=alpha, fan_in_fan_out=False, dropout_rate=0.25),
             nn.LeakyReLU(),
-            LoRALinear((token_length * output_n) // 2, (token_length * output_n), r=r, alpha=alpha),
+            LinearLoRA(existing_linear_layer_2, r=r, alpha=alpha, fan_in_fan_out=False, dropout_rate=0.25),
             nn.LeakyReLU())
 
     def forward(self, x):
